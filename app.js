64ef07d9a71fbb87853f53d6c4cf9d04649dcfa0
Miles <milesmaratas29@gmail.com>
2024-05-09 19:09:06 +0800

    users

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const mongoose = require("mongoose");
 const app = express();
 const multer = require("multer");
 const morgan = require("morgan");
+const bcrypt = require("bcrypt");
 
 const LocationDetails = require("./api/models/location");
+const User = require("./api/models/user");
 
 app.use(express.json());
 app.listen(3000);
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
-
+// para kuha sa detail sa location
 app.get("/details", (req, res, next) => {
   LocationDetails.find()
     .select("name price _id location time locationImage description")
@@ -69,12 +71,15 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
+app.post;
 const upload = multer({
   storage: storage,
   limits: { fileSize: 1024 * 1024 * 5 },
   fileFilter: fileFilter,
 });
+//very gubooooooooooooooooootttttt
 
+/// create details sa location
 app.post(
   "/createLocation",
   upload.single("locationImage"),
@@ -86,7 +91,6 @@ app.post(
         },
       });
     }
-
     console.log(req.file);
     const locationDetails = new LocationDetails({
       _id: new mongoose.Types.ObjectId(),
@@ -124,6 +128,103 @@ app.post(
       });
   }
 );
+// para sign up ni
+app.post("/signup", upload.single("profileImage"), (req, res, next) => {
+  if (!req.file) {
+    return res.status(400).json({
+      error: {
+        message: "No file uploaded",
+      },
+    });
+  }
+  if (!req.body.password || req.body.password.trim().length === 0) {
+    return res.status(400).json({
+      message: "Password cannot be empty",
+    });
+  }
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Mail exists",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              lastName: req.body.lastName,
+              firstName: req.body.firstName,
+              phoneNumber: req.body.phoneNumber,
+              email: req.body.email,
+              profileImage: req.file.path,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
+});
+///// para log-in
+app.get("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          return res.status(200).json({
+            message: "Auth successful",
+            user: {
+              _id: user._id,
+              email: user.email,
+              lastName: user.lastName,
+              firstName: user.firstName,
+              phoneNumber: user.phoneNumber,
+              profileImage: user.profileImage,
+            },
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 mongoose.connect(
   "mongodb+srv://maratasmiles9:putoflan123@intprogapi.41polww.mongodb.net/Itinerawise?retryWrites=true&w=majority&appName=intprogAPI"
@@ -149,7 +250,6 @@ app.use((req, res, next) => {
   error.status = 404;
   next(error);
 });
-
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
