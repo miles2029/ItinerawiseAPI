da926bfd32f771811f9d7f0f143264e3662fdd58
Miles <milesmaratas29@gmail.com>
2024-05-14 02:54:38 +0800

    Price  number change to  String

--- api/models/location.js
+++ api/models/location.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const locationSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   location: { type: String, required: true },
   time: { type: String, required: true },
   locationImage: { type: String, required: true },
